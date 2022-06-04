0f132c28491b4bdf936c211e5d1bb2f1d7b51525
Mustafa Köroğlu <mestefe119@gmail.com>
2022-06-04 23:57:10 +0300

    Create AddServicingToMenuRequest.java

---
+++ FoundOutBackendRestaurants/src/main/java/com/mustafak01/foundoutbackendrestaurants/model/requests/AddServicingToMenuRequest.java
@@ -0,0 +1,19 @@
+package com.mustafak01.foundoutbackendrestaurants.model.requests;
+
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class AddServicingToMenuRequest {
+    private final Long id;
+    private final Long userId;
+    private final String servicingName;
+    private final String servicingPrice;
+    private final String menuName;
+}
