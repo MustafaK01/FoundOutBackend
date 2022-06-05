aa84db8abd48f5f352d5e2a38e347853bab9a4e2
Mustafa Köroğlu <mestefe119@gmail.com>
2022-06-05 19:04:19 +0300

    Create MenuServicingDto.java

---
+++ FoundOutBackendRestaurants/src/main/java/com/mustafak01/foundoutbackendrestaurants/model/dtos/MenuServicingDto.java
@@ -0,0 +1,17 @@
+package com.mustafak01.foundoutbackendrestaurants.model.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MenuServicingDto {
+
+    private Long servicingId;
+    private String servicingName;
+    private String servicingPrice;
+    private Long menuId;
+
+}
