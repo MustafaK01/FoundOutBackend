6f1a02180d55d55491924185a5fb144941dfd068
Mustafa Köroğlu <mestefe119@gmail.com>
2022-06-09 05:14:09 +0300

    Add
    Added for identity verification

--- FoundOutBackendRestaurants/src/main/java/com/mustafak01/foundoutbackendrestaurants/FoundOutBackendRestaurantsApplication.java
+++ FoundOutBackendRestaurants/src/main/java/com/mustafak01/foundoutbackendrestaurants/FoundOutBackendRestaurantsApplication.java
@@ -1,5 +1,6 @@
 package com.mustafak01.foundoutbackendrestaurants;
 
+import com.mustafak01.foundoutbackendrestaurants.mernis.KPSPublicSoap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FoundOutBackendRestaurantsApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+//		KPSPublicSoap client = new KPSPublicSoap();
+//		Long tc = 65879865658L;
+//		Boolean isTrue = client.TCKimlikNoDogrula(00000000000L,"İSİM","SOYİSİM",DOGUM_TARİHİ);
+//		System.out.println(isTrue);
 		SpringApplication.run(FoundOutBackendRestaurantsApplication.class, args);
 	}
 
